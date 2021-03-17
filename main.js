cb6a56516ed5df560f38239a3310647abf5e9b6f
Klapotin <Klapotin@users.noreply.github.com>
2021-03-17 19:35:06 -0400

    game loops

---
+++ main.js
@@ -0,0 +1,14 @@
+canvasDimensions, canvas, ctx; // this just prevents intellisense from being stupid (can be removed)
+
+// SETTINGS
+const game = {
+	FPS: 60
+};
+
+// GAME VARS
+
+// GAME LOOPS
+game.update = function (dt) {};
+game.render = function () {};
+
+// OTHER FUNCS
